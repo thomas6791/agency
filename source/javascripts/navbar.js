fe0c5ccf5414e771deadbbb956e7dbb285e55e9c
thomas hunckler <hunckler.thomas@hotmail.fr>
2018-10-11 22:42:50 +0200

    compil js

--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -1,7 +1,8 @@
 function classToggle() {
-  const navs = document.querySelectorAll('.Navbar__Items')
+  var navs = document.querySelectorAll('.Navbar__Items');
 
-  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  navs.forEach(function (nav) {
+    return nav.classList.toggle('Navbar__ToggleShow');
+  });
 }
-document.querySelector('.Navbar__Link-toggle')
-  .addEventListener('click', classToggle);
+document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
