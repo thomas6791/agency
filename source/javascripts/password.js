668dae81b737935ad9747c13b1b562992d53c55f
thomas hunckler <hunckler.thomas@hotmail.fr>
2018-11-20 08:56:38 +0100

    add password option

---
+++ source/javascripts/password.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+  <!--hide
+  var password;
+  var pass1="12345";
+  password=prompt('Enter password');
+  if (password==pass1)
+    alter('Correct password, click Ok to enter.');
+  else
+  {
+    window.location="http://localhost:4567/creation-site-internet.html";
+  }
+  //-->
+</script>
